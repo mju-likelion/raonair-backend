09718e0add0b813a5e4a35b2d94a6a7f8a47ec82
junseokieo <ccobaltbluee@gmail.com>
2021-09-30 23:59:35 +0900

    추가: user 데이터 입력

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const path = require('path');
 const morgan = require('morgan');
 const http = require('http');
 
-const { sequelize } =require('./models');
+const { sequelize } = require('./models');
+const user = require('./models/user');
 
 const app = express();
+
 app.set('port', process.env.PORT || 8000);
+
 sequelize.sync({ force: false })
     .then(() => {
         console.log('데이터베이스 연결 성공');
@@ -17,16 +20,25 @@ sequelize.sync({ force: false })
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+//     //res.send('Hello World');
+// });
+app.get('/', (req, res, next) => {
+    user.create({
+        name: "fly",
+    });
+    console.log('입력 성공');
+})
 
+// 404 middleware
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
 });
 
+// error middleware
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'development' ? err : {};
