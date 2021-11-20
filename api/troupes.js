64df854e6eaba9ab81aa122cb72572b2f0fb27e2
junseokieo <ccobaltbluee@gmail.com>
2021-11-20 21:29:48 +0900

    추가: 극단 라우터 생성

--- api/troupes.js
+++ api/troupes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const Troupe = require('../models/troupe');
 const troupes = express.Router();
 
+// api/troupes/
 troupes.get('/:id', (req, res, next) => {
+
     res.send(req.params.id);
+
 });
 
 module.exports = troupes;
